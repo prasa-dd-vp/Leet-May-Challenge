6217b7575cd6226b02b41a52993804be2b1480ce
prasa-dd-vp <prasad.venkatesh90@gmail.com>
2020-05-02 20:36:45 +0530

    Day 2 Linear search problem

---
+++ JewelsAndStones.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int ans = 0;
+        for (int i=0; i<S.length(); i++){
+            if(J.indexOf(S.charAt(i)) != -1){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
